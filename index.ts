418be735da6c718ac7849dbf3037f80af68fd5d8
Max Anashenko <malyby.anashenko@yandex.ru>
2023-05-13 22:05:02 +0500

    change of structure

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 
-// import { lec1 } from "./src/mod1_lec1";
+// import { lec1 } from "./src/mod1/lec1";
 // lec1();
 
-import { lec2 } from "./src/mod1_lec2";
+import { lec2 } from "./src/mod1/lec2";
 
 lec2().inversed('https://img5.goodfon.ru/original/800x480/f/1e/kotenok-sidit-pushistyi-trava.jpg');
 lec2().grayscale('https://img5.goodfon.ru/original/800x480/f/1e/kotenok-sidit-pushistyi-trava.jpg');
